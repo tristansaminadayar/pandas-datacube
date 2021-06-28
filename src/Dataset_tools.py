3322efc27340262a22d597e40ff3deb14d4db94f
tristansaminadayar <tristan.saminadayar@gmail.com>
2021-06-28 11:48:13 +0200

    Ajout de la separation entre dimension/mesure

--- src/Dataset_tools.py
+++ src/Dataset_tools.py
@@ -1,5 +1,6 @@
 import re
 import time as tm
+from typing import Any
 
 import pandas as pd
 from IPython.core.display import display
@@ -16,11 +17,11 @@ def add_progress_bar(fun: callable) -> callable:
     :return: The modified function
     """
 
-    def function_modif(*args, **kwargs):
+    def function_modif(*args, **kwargs) -> Any:
         progress_bar: widgets.IntProgress = widgets.IntProgress(bar_style='success', description='Loading:')
         display(progress_bar)
         kwargs['widget'] = progress_bar
-        ret: callable = fun(*args, **kwargs)
+        ret: Any = fun(*args, **kwargs)
         progress_bar.close()
         return ret
 
@@ -38,8 +39,10 @@ def get_datasets(endpoint: str, verbose: bool = False, widget: widgets.IntProgre
     :return: The data frame of all datasets available names and their description
     """
 
-    query: str = ("SELECT DISTINCT ?dataset ?commentaire WHERE {?dataset a <http://purl.org/linked-data/cube#DataSet> "
-                  "OPTIONAL {?dataset <http://www.w3.org/2000/01/rdf-schema#comment> ?commentaire}}"
+    query: str = ("SELECT DISTINCT ?structure ?dataset ?commentaire WHERE "
+                  "{?dataset a <http://purl.org/linked-data/cube#DataSet> . "
+                  "?dataset <http://purl.org/linked-data/cube#structure> ?structure "
+                  "OPTIONAL {?dataset <http://www.w3.org/2000/01/rdf-schema#comment> ?commentaire }}"
                   )
 
     if verbose:
@@ -55,28 +58,29 @@ def get_datasets(endpoint: str, verbose: bool = False, widget: widgets.IntProgre
 
 
 @add_progress_bar
-def get_features(endpoint: str, dataset_name: str, widget: widgets.IntProgress = None,
+def get_features(endpoint: str, dataset_structure: str, widget: widgets.IntProgress = None,
                  verbose: bool = False) -> pd.DataFrame:
     """
     Get all features available names on a dataset.
 
     :param verbose: If the detail text will be displayed
     :param endpoint: The address of the SPARQL server
-    :param dataset_name: The name of the dataset where you want to have its features
+    :param dataset_structure: The URI of the structure of the dataset where you want to have its features
     :param widget: If the detail widget will be displayed
     :return: The data frame of all datasets features names available
     """
-    query: str = ("select ?property where { { select ?item where {?item <http://purl.org/linked-data/cube#dataSet> "
-                  f"<{dataset_name}> }} LIMIT 1 }} ?item ?property ?value . filter ( ?property not in "
-                  "(<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>) ) }"
-                  )
+
+    query: str = f"""select distinct ?type ?property where {{
+        {{ select ?item where {{ <{dataset_structure}> <http://purl.org/linked-data/cube#component> ?item }} }}
+        ?item ?type ?property }}"""
 
     result: pd.DataFrame = SPARQLquery(endpoint, query, widget=widget, verbose=verbose).do_query()
-    return result
+
+    return result[result['type'].isin(["http://purl.org/linked-data/cube#dimension", "http://purl.org/linked-data/cube#measure"])]
 
 
 @add_progress_bar
-def download_dataset(endpoint: str, dataset_name: str, features_names: list[str],
+def download_dataset(endpoint: str, dataset_name: str, dimensions: list[str], mesures: list[str],
                      widget: widgets.IntProgress = None, verbose: bool = False) -> pd.DataFrame:
     """
     Download and return all selected features of a dataset
@@ -84,17 +88,24 @@ def download_dataset(endpoint: str, dataset_name: str, features_names: list[str]
     :param verbose: If the detail text will be displayed
     :param endpoint: The address of the SPARQL server
     :param dataset_name: The name of the dataset where you want to download its features
-    :param features_names: The names of features to download
+    :param mesures: The names of mesures to download
+    :param dimensions: The names of dimensions to download
     :param widget: If the detail widget will be displayed
     :return: The data frame of selected and downloaded characteristics of a dataset
     """
 
     # We will build the query
     query: str = "SELECT "
-    vars_list: list[str] = [re.sub('[^A-Za-z0-9]+', '', item.split('#')[-1].split('/')[-1]) for item in features_names]
+    mesures_name: list[str] = [re.sub('[^A-Za-z0-9]+', '', item.split('#')[-1].split('/')[-1]) for item in mesures]
+    dimensions_name: list[str] = [re.sub('[^A-Za-z0-9]+', '', item.split('#')[-1].split('/')[-1]) for item in
+                                  dimensions]
+    vars_list: list[str] = dimensions_name + mesures_name
+    url_list: list[str] = dimensions + mesures
+
     query += " ".join([f"?{item}" for item in vars_list])
     query += f" WHERE {'{'} ?o <http://purl.org/linked-data/cube#dataSet> <{dataset_name}> . "
-    query += " ".join([f"?o <{uri}> ?{name} ." for uri, name in zip(features_names, vars_list)])
+    query += " ".join([f"OPTIONAL{{ ?o <{uri}> ?{name} }}" for uri, name in zip(url_list, vars_list)])
     query += " } "
     # Do the query
-    return SPARQLquery(endpoint, query, widget=widget, verbose=verbose).do_query()
+
+    return SPARQLquery(endpoint, query, widget=widget, verbose=verbose).do_query().set_index(dimensions_name)
