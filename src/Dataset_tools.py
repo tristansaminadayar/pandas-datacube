2d11a1c65d97ac4456df68b2849e0b7b0c9cb5a2
tristansaminadayar <tristan.saminadayar@gmail.com>
2021-07-01 15:48:28 +0200

    added download_dataset

--- src/Dataset_tools.py
+++ src/Dataset_tools.py
@@ -1,6 +1,6 @@
 import re
 import time as tm
-from typing import Any, Tuple, Union
+from typing import Any, Union
 
 import pandas as pd
 from IPython.core.display import display
@@ -26,6 +26,36 @@ def add_progress_bar(fun: callable) -> callable:
         return ret
 
     return function_modif
+
+
+def expand_name(word: str, prefixes: dict[str]) -> str:
+    """
+    Function that transforms the prefix using a dictionary of conversions
+
+    Exemple:
+
+    expand_name("dbo:PopulatedPlace/areaTotal", {'dbo': 'https://dbpedia.org/ontology/'})
+    > "https://dbpedia.org/ontology/PopulatedPlace/areaTotal"
+
+
+    :param word: The word to expand
+    :param prefixes: The dictionary of conversions
+    :return: the word expanded
+    """
+    name = word.split(':')
+
+    if len(name) == 1:
+        return name[0]
+    elif len(name) == 2:
+        expanded: str = ""
+        for key in prefixes:
+            if key == name[0]:
+                expanded = prefixes[key]
+        if expanded == "":
+            raise KeyError(f"{name[0]} does not belong to the known prefixes and cannot be expanded")
+        return expanded + name[1]
+    else:
+        raise ValueError(f"There are several ':' in the word expanded {word}")
 
 
 @add_progress_bar
@@ -76,13 +106,22 @@ def get_features(endpoint: str, dataset_name: str, widget: widgets.IntProgress =
     return SPARQLquery(endpoint, query, widget=widget, verbose=verbose).do_query()
 
 
-def transform_features(features: pd.DataFrame) -> Tuple[list[str], list[str]]:
-    unique_features: list[str] = features['item'].unique()
-    order: bool = False
+def transform_features(features: pd.DataFrame) -> tuple[list[str], list[str]]:
+    """
+    Transform the dataframe obtained by `get_features` to a list of all measures and dimensions,
+    if the order component is in the cube, the dimensions list will be sorted
+
+    :param features: The dataframe obtained by `get_features`
+    :return: The list of all dimensions and the list of all measures
+    """
+
+    unique_features: list[str] = features['item'].unique()  # Get all unique measure/dimension of the dataset
+
+    order: bool = False  # Check if the attribut order is filled
     if "http://purl.org/linked-data/cube#order" in features['type']:
         order = True
 
-    dimensions: list[Union[str, Tuple[str, int]]] = []
+    dimensions: list[Union[str, tuple[str, int]]] = []
     measures: list[str] = []
 
     for feature in unique_features:
@@ -91,6 +130,7 @@ def transform_features(features: pd.DataFrame) -> Tuple[list[str], list[str]]:
             ['http://purl.org/linked-data/cube#dimension',
              'http://purl.org/linked-data/cube#measure',
              'http://purl.org/linked-data/cube#attribute'])]
+
         if compo['type'].values[0] == 'http://purl.org/linked-data/cube#measure':
             measures.append(compo['property'].values[0])
         elif compo['type'].values[0] == 'http://purl.org/linked-data/cube#dimension':
@@ -137,3 +177,49 @@ def download_dataset(endpoint: str, dataset_name: str, dimensions: list[str], me
 
     # Do the query
     return SPARQLquery(endpoint, query, widget=widget, verbose=verbose).do_query().set_index(dimensions_name)
+
+
+def from_datacube(sparql_endpoint: str, dataset_name: str = "", dimensions: list[str] = None,
+                  measures: list[str] = None, dtypes: dict[type] = None, prefixes: dict[str] = None) -> pd.DataFrame:
+    """
+    Function to download a datacube
+
+
+    :param prefixes: The dictionary of the namespace prefixes
+    :param sparql_endpoint: The URL of the SPARQL endpoint
+    :param dataset_name: The name of the datacube (default: a random datacube)
+    :param dimensions: The dimensions of the datacube to download (default: all dimensions)
+    :param measures: The measures of the datacube to download (default: all measures)
+    :param dtypes: The type of measures (default: str)
+    :return: A dataframe containing the result, with measures indexed by dimensions
+    """
+
+    # The case where prefixes has been given
+    if prefixes is not None:
+        dataset_name = expand_name(dataset_name, prefixes)
+        measures = [expand_name(measure, prefixes) for measure in measures]
+        dimensions = [expand_name(dimension, prefixes) for dimension in dimensions]
+
+    # The case where no dataset_name has been given
+    if dataset_name == "":
+        list_datasets: pd.DataFrame = get_datasets(sparql_endpoint)
+        if len(list_datasets) == 0:
+            raise KeyError(f"No datasets was found in this endpoint ({sparql_endpoint})")
+        else:
+            dataset_name = list_datasets['dataset'].values[0]
+
+    # The case where no dimension or/and measures has been given
+    if len(measures) == 0 or len(dimensions) == 0:
+        full_dim, full_msr = transform_features(get_features(sparql_endpoint, dataset_name))
+        if len(dimensions) == 0:
+            dimension = full_dim
+        if len(measures) == 0:
+            measures = full_dim
+
+    dataframe: pd.DataFrame = download_dataset(sparql_endpoint, dataset_name, dimensions, measures)
+
+    # The case where types has been given
+    for key in dtypes:
+        dataframe[key] = dataframe[key].astype(dtypes[key])
+
+    return dataframe
