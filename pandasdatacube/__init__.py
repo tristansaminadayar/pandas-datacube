c0149e7805b4cd73d87a6849d6dff5bbc90b38b6
tristansaminadayar <tristan.saminadayar@gmail.com>
2021-07-12 19:45:16 +0200

    Added doc

--- pandasdatacube/__init__.py
+++ pandasdatacube/__init__.py
@@ -1,4 +1,20 @@
 from .pandasdatacube import *
 from .SPARQL_query import *
 
+__doc__ = """
+Pandas Datacube - A python package to get datacube to dataframe 
+using sparql
+=====================================================================
+
+**pandas-datacube** is a python package allowing to convert and  
+download a datacube from a remote source using 
+[SPARQL](https://www.w3.org/TR/sparql11-overview/) queries and to 
+obtain a pandas dataframe
+
+Main Features
+-------------
+  - automatically detects all datasets present in a SPARQL endpoint
+  - detects all dimensions and measures of a dataset
+  - download a dataset with the chosen characteristics
+"""
 
